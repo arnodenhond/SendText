5460414ad9e43afc32d2bb5af19a1c80864705ba
arnodenhond <arnodenhond@gmail.com>
2016-09-03 17:37:33 +0200

    tablet layouts

---
+++ app/src/main/java/arnodenhond/sendtext/DisableClipboard.java
@@ -0,0 +1,12 @@
+package arnodenhond.sendtext;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DisableClipboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ app/src/main/java/arnodenhond/sendtext/SpeakText.java
@@ -0,0 +1,7 @@
+package arnodenhond.sendtext;
+
+/**
+ * Created by arnodenhond on 02/09/16.
+ */
+public class SpeakText {
+}
